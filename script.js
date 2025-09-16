b56b29db662c4765023f335b9e36eb259e5c8563
Olivia <aguado.148024@e.unavarra.es>
2025-09-16 17:10:01 +0200

    Primer commit - archivos iniciales

---
+++ script.js
@@ -0,0 +1,14 @@
+// Obtengo referencias del texto introducido y del botón
+const input = document.getElementById('title');
+const button = document.getElementById('searchBtn')
+
+// Inicio proceso de cuado se hace click sobre el botón
+button.addEventListener('click', () => {
+    const titulo = input.value;
+
+
+    // Mi clave es 10332218
+    url = "http://www.omdbapi.com/?apikey=[10332218]&"
+
+    //console.log('Titulo: ', titulo)
+})
