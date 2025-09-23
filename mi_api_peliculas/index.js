312e43e7a52dc9bd8ca101c461b64cf0b156ee3a
Olivia <aguado.148024@e.unavarra.es>
2025-09-23 17:48:10 +0200

    Creado y probado el primer endpoint

--- mi_api_peliculas/index.js
+++ mi_api_peliculas/index.js
@@ -1,6 +1,9 @@
 // Importamos Express
 const express = require('express');
 
+// Importo fs para leer archivos json
+const fs = require('fs');
+
 // Creamos la aplicación
 const app = express();
 
@@ -14,6 +17,39 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('API de Películas funcionando');
 });
+
+// Endpoint Buscar una película
+app.get('/peliculas/:id', (req, res) => {
+  const id = req.params.id;
+
+  fs.readFile('peliculas.json', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).json({error: 'Error leyendo el archivo'});
+      return;
+    }
+
+    const peliculas = JSON.parse(data);
+    const pelicula = peliculas.find(p => p.id === id);
+
+    if (!pelicula) {
+      res.status(404).json({ error: 'Película no encontrada' });
+      return;
+    }
+
+    res.json(pelicula);
+  });
+});
+
+// Endpoint Crear una pelicula
+// Endpoint borrar una pelicula
+// Endpoint cambiar nombre
+// Endpoint cambiar año
+// Endpoint añadir actor
+// Endpoint quitar actor
+// Endpoint crear un actor
+// Endpoint borrar un actor
+// Endpoint modificar nombre de actor
+// Endpoint modificar año de actor
 
 // Iniciar el servidor
 app.listen(PORT, () => {
--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 // Obtengo referencias del texto introducido y del botón
 const input = document.getElementById('title');
-const button = document.getElementById('searchBtn')
+const button = document.getElementById('searchBtn');
+const resultDiv = document.getElementById('result');
 
 // Inicio proceso de cuado se hace click sobre el botón
-button.addEventListener('click', () => {
+button.addEventListener('click', async () => {
     const titulo = input.value;
 
+    const apikey = "10332218";
+    const url = `https://www.omdbapi.com/?apikey=${apikey}&t=${encodeURIComponent(titulo)}`;
+    
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            // Si no encuentra la peli
+            if (data.Response === "False") {
+                resultDiv.textContent = "Pelicula no encontrada";
+                return;
+            }
 
-    // Mi clave es 10332218
-    url = "http://www.omdbapi.com/?apikey=[10332218]&"
+            // Si encuentra muestro año y director
+            resultDiv.innerHTML = `
+                <p>Director: ${data.Director}</p>
+                <p>Year: ${data.Year}</p>
+            `;
+        })
+        .catch(err => {
+            console.error("Error en la petición:", err);
+            resultDiv.textContent = "Error al consultar la API";
+        });
 
-    //console.log('Titulo: ', titulo)
+    
+
+
+    
 })
